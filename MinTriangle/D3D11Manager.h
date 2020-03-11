39b75378ee0c73548bb1ac3f27c51570e8f6b3a5
Xinzhao Zhu <zhuxinzhao@outlook.com>
2020-03-11 14:40:51 +0800

    Port to use D3D11On12 instead of D3D11, now it could work.
    The first sample has pass. It could draw the triangle corrctly, but the
    dtor part is not refined, so it may not exit successfully.

--- MinTriangle/D3D11Manager.h
+++ MinTriangle/D3D11Manager.h
@@ -2,12 +2,23 @@
 #include <memory>
 #include <wrl/client.h>
 
+#include <d3d11on12.h>
+#include <d3d12.h>
+
 
 class D3D11Manager
 {
-    Microsoft::WRL::ComPtr<struct IDXGISwapChain> m_pSwapChain;
+    Microsoft::WRL::ComPtr<struct IDXGISwapChain3> m_pSwapChain; // use this swap-chain type
+
     Microsoft::WRL::ComPtr<struct ID3D11Device> m_pDevice;
+
+    Microsoft::WRL::ComPtr<ID3D12Device> m_pD3D12Device;
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_pD3D12CommandQueue;
+    Microsoft::WRL::ComPtr<ID3D11On12Device> m_pD3D11On12Device;
+
     Microsoft::WRL::ComPtr<struct ID3D11DeviceContext> m_pDeviceContext;
+
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> m_pBackBuffers[2]; // the wrapped back buffer number, here we hard code it to double-buffer
 
     std::shared_ptr<class RenderTarget> m_renderTarget;
     std::shared_ptr<class Shader> m_shader;
